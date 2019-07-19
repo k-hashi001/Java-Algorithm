3d2b1fe09502ae147cf4161eb565c450235040c7
k-hashi001 <50068541+k-hashi001@users.noreply.github.com>
2019-07-19 14:53:06 +0900

    Create bubble_sort.java

---
+++ bubble_sort.java
@@ -0,0 +1,35 @@
+//20個数字を入力させ、バブルソートする
+
+package newkadai;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sort_main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] nums = new int[20];
+		
+		System.out.println("20個の数字を入力してください");
+		for (int i = 0; i < nums.length; i++) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String str = br.readLine();
+			nums[i] = Integer.parseInt(str);
+		}
+		
+		for(int i=nums.length-1 ; i > 0; i--) {
+			for(int j=0;j<i; j++) {
+				if(nums[j]>nums[j+1]) {
+					int t= nums[j];
+					nums[j]=nums[j+1];
+					nums[j+1]=t;
+				}
+			}
+		}for(int a=0;a<nums.length;a++) {
+			System.out.print(nums[a]);
+		}
+		
+	}
+}
