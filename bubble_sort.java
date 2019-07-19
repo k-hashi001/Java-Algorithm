0d051ff403e653e6b993d2ac60a9f829a2e5fd69
k-hashi001 <50068541+k-hashi001@users.noreply.github.com>
2019-07-19 14:55:19 +0900

    Update bubble_sort.java

--- bubble_sort.java
+++ bubble_sort.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class sort_main {
+public class sort {
 
 	public static void main(String[] args) throws IOException {
 		// TODO 自動生成されたメソッド・スタブ
