e363ae9bb17e7170b23aaa6ac8b14b2122c41bd6
k-hashi001 <50068541+k-hashi001@users.noreply.github.com>
2019-07-20 23:16:58 +0900

    Update Binary_Search.java

--- Binary_Search.java
+++ Binary_Search.java
@@ -15,33 +15,36 @@ public class Binary_search {
 		int Right = nums.length - 1;
 		int Middle = (Left + Right) / 2;
 		boolean Answer = false;
-		for (int i = 0; i < nums.length; i++) {
+		try {
+			for (int i = 0; i < nums.length; i++) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				String str1 = br.readLine();
+				nums[i] = Integer.parseInt(str1);
+			}
+			System.out.println("検索する数字を入力してください");
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String str1 = br.readLine();
-			nums[i] = Integer.parseInt(str1);
-		}
-		System.out.println("検索する数字を入力してください");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str2 = br.readLine();
-		Search = Integer.parseInt(str2);
-		while (Left <= Right) {
-			if (nums[Middle] == Search) {
-				Answer = true;
-				break;
-			}else {
-				if (Search < nums[Middle]) {
-					Right = Middle - 1;
+			String str2 = br.readLine();
+			Search = Integer.parseInt(str2);
+			while (Left <= Right) {
+				if (nums[Middle] == Search) {
+					Answer = true;
+					break;
 				} else {
-					Left = Middle + 1;
+					if (Search < nums[Middle]) {
+						Right = Middle - 1;
+					} else {
+						Left = Middle + 1;
+					}
 				}
-       }
 				Middle = (Left + Right) / 2;
 			}
-			if(Answer==true) {
-		System.out.println((Middle+1)+"番目に見つかりました。");
-			}else {
+			if (Answer == true) {
+				System.out.println((Middle + 1) + "番目に見つかりました。");
+			} else {
 				System.out.println("見つかりませんでした。");
 			}
+		} catch (Exception e) {
+			System.out.println("エラーが発生しました。");
 		}
 	}
-
+}
