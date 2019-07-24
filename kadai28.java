adf35b268d40c7ccb3684e80fe70e0d034166cc9
k-hashi001 <50068541+k-hashi001@users.noreply.github.com>
2019-07-24 11:30:15 +0900

    Update kadai28.java

--- kadai28.java
+++ kadai28.java
@@ -17,40 +17,39 @@ public class Kadai28_main {
 		// TODO 自動生成されたメソッド・スタブ
 		String[] charas = new String[10];
 		List<String> list = new ArrayList<String>();
-		int count=0;
-		String same=null;
+		int count = 0;
+		String same = null;
 		System.out.println("9回文字を入力してください。");
 		try {
-		for (int i = 0; i < 9; i++) {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String chara = br.readLine();
-			charas[i] = chara;
-			
-		}
-		charas[9]="";
-		for(int i=0;i<10;i++) {
-			  if(i==0) {
-				  count=1;
-				  same=charas[0];
-			  }else if (charas[i].equals(same)){
-				  count++;
-			  }else if(!(charas[i].equals(same))&&count>=3) {
-				  String str=Integer.toString(count);
-				  list.add(same+str);
-				  same=charas[i];
-				  count=1;
-			  }else {
-				  list.add(same);
-				  same=charas[i];
-			  }
+			for (int i = 0; i < 9; i++) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				String chara = br.readLine();
+				charas[i] = chara;
+
 			}
-		for (int i=0; i<list.size(); ++i)
-		{
-		    System.out.print(list.get(i));
-		}
-		
-			}catch(Exception e) {
-				System.out.println("エラーが発生しました。");
+			charas[9] = "";
+			for (int i = 0; i < 10; i++) {
+				if (i == 0) {
+					count = 1;
+					same = charas[0];
+				} else if (charas[i].equals(same)) {
+					count++;
+				} else if (!(charas[i].equals(same)) && count >= 3) {
+					String str = Integer.toString(count);
+					list.add(same + str);
+					same = charas[i];
+					count = 1;
+				} else {
+					list.add(same);
+					same = charas[i];
+				}
 			}
+			for (int i = 0; i < list.size(); ++i) {
+				System.out.print(list.get(i));
+			}
+
+		} catch (Exception e) {
+			System.out.println("エラーが発生しました。");
 		}
 	}
+}
